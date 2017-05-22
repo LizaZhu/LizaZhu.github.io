c06fc6d2b06a4e87ad1f466e3356a39a92cd5e95
Liza Zhuykova <zhuykova7@gmail.com>
2017-05-22 20:10:18 +0500

    ourteam__item_active

--- burger/js/slider.js
+++ burger/js/slider.js
@@ -10,12 +10,12 @@ $(document).ready(function(){
 
 		if (item.hasClass('ourteam__name_active')) {
 			personBlock.css('display', 'none');
-			item.removeClass('ourteam__name_active');
+			item.removeClass('ourteam__item_active');
 		} else {
-			item.addClass('ourteam__name_active');
+			item.addClass('ourteam__item_active');
 			personBlock.css('display', 'block');
 			otherPersonInf.css('display', 'none');
-			otherPerson.removeClass('ourteam__name_active')
+			otherPerson.removeClass('ourteam__item_active')
 		}
 	})
 })
