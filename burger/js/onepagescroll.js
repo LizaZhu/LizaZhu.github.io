50c8f90182cc310e2b96972be2679feea9006de7
Liza Zhuykova <zhuykova7@gmail.com>
2017-05-25 23:55:14 +0500

    onepagescroll

---
+++ burger/js/onepagescroll.js
@@ -0,0 +1,44 @@
+(function() {
+	var display = $('.maincontent');
+	var sections =$('.section');
+	var inScroll = false;
+
+	var performTransition = function(sectEq){
+		if (inScroll) return;
+		inScroll = true;
+		var position = (sectEq * -100) + '%';
+
+		sections.eq(sectEq).addClass('active')
+			.siblings().removeClass('active')
+
+		display.css({
+			'transform':'translate(0,'+ position + ')'
+		})
+
+		setTimeout(function(){
+			inScroll = false
+			$('.fixed-menu__item').eq(sectEq).addClass('fixed-menu__item_active')
+					.siblings().removeClass('fixed-menu__item_active');
+		}, 1300)
+	}
+
+	$('.wrapper').on('wheel',function(e){
+		var activeSection = sections.filter('.active');
+		var nextSection = activeSection.next();
+		var prevSection = activeSection.prev();
+		if (e.originalEvent.deltaY > 0 && nextSection.length){//скролл вниз
+			performTransition(nextSection.index())
+		}
+		if (e.originalEvent.deltaY < 0 && prevSection.length){//скролл вверх
+			performTransition(prevSection.index())
+		}
+	})
+
+	$('.nav__link, .fixed-menu__link, .down, .link__order').on('click', function(e){
+		e.preventDefault();
+		var href = parseInt($(this).attr('href'));
+		performTransition(href)
+	})
+	
+
+}())
