3a2174d90fe943d2f500bbfcd3312397d5e9296a
LizaZhu <zhuykova7@gmail.com>
2017-06-01 17:56:15 +0500

    ф

---
+++ form/form-handler.php
@@ -0,0 +1,10 @@
+<?php
+// print_r($_REQUEST)
+// $name = $_POST['name'];
+// $data = $_POST['data'];
+// $age = rand(15,30);
+
+// echo "name: ".$name.".$age"."<br>";
+// echo "data: ".$data;
+?>
+
